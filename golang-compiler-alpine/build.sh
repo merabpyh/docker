6723a6d6dbbc2f52f2259ddb9b08ca6f84c13b41
Roman Abrosimov <merabpyh@yandex.ru>
2017-06-12 05:21:42 -0400

    add build.sh

---
+++ golang-compiler-alpine/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# USR/REPO is a your github repo
+USR=XXX
+REPO=XXX
+
+# DUSR/DREPO is a your dockerhub repo
+DUSR=$USR
+DREPO=XXX
+
+# For option -v HOSTdir:CONTdir
+INDIR=$(pwd)/output
+OUTDIR=/mnt/output
+
+#echo "Create output dir"
+mkdir ./output
+
+#echo "Build image"
+docker build -t $DUSR/$DREPO:latest $DDIR
+
+#echo "Run container"
+docker run \
+	-v $INDIR:$OUTDIR \
+	-e USR=$USR \
+	-e REPO=$REPO \
+	--rm $DUSR/$DREPO:latest 
+
