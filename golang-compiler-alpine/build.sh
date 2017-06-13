13682f6ed1b8bbb4772dc042d7ac86d5458707c2
Roman <merabpyh@yandex.ru>
2017-06-13 16:13:29 +0300

    upd build.sh
    cleanup

--- golang-compiler-alpine/build.sh
+++ golang-compiler-alpine/build.sh
@@ -2,35 +2,17 @@
 
 # Run from the directory with Dockerfile
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 1 ]
 then
-  echo "Usage: ./build.sh \"USER\" \"REPO without user\""
-  echo "Like: ./build.sh merabpyh checker-client"
-  echo "Be careful, do not use special characters in the user or repo name"
+  echo "Usage: ./build.sh REIGSTRY"
   exit 1
 fi
 
-# USR/REPO is a your github repo
+# it's your dockerhub repo
 USR=$1
-REPO=$2
-
-# USR/DREPO is a your dockerhub repo
-DREPO=alpine.go.compiler
-
-# For option -v HOSTdir:CONTdir
-INDIR=$(pwd)/output
-OUTDIR=/mnt/output
 
 #echo "Create output dir"
-mkdir ./output
+if [ ! -d "output" ]; then mkdir output; fi
 
 #echo "Build image"
-docker build -t $USR/$DREPO:latest .
-
-#echo "Run container"
-docker run \
-	-v $INDIR:$OUTDIR \
-	-e USR=$USR \
-	-e REPO=$REPO \
-	--rm $USR/$DREPO:latest 
-
+docker build -t $USR/alpine.go.compiler:latest .
