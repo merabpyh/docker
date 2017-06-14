2ae65b8e06006548216408646dfdb821b4320b1b
Roman <merabpyh@yandex.ru>
2017-06-14 16:32:42 +0300

    upd run.sh
    add some comments

--- golang-compiler-alpine/run.sh
+++ golang-compiler-alpine/run.sh
@@ -2,6 +2,7 @@
 
 # Run from the directory with Dockerfile
 
+# Check arg
 if [ "$#" -ne 1 ]
 then
   echo "Usage: ./run.sh USER/REPO"
@@ -10,10 +11,8 @@ then
 fi
 
 # USR/REPO is a your github repo
-
 USR=$(echo $1 | awk -F/ '{print $1}')
 REPO=$(echo $1 | awk -F/ '{print $2}')
-
 
 # USR/DREPO is a your dockerhub repo
 DREPO=alpine.go.compiler
@@ -30,5 +29,6 @@ docker run \
 	-v $INDIR:$OUTDIR \
 	-e USR=$USR \
 	-e REPO=$REPO \
-	--rm $USR/$DREPO:latest
+#	--rm $USR/$DREPO:latest
+	--rm $DREPO:latest
   
