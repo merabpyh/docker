1fefde6dae3ada9bbfb55475113b14e99363d0b8
Roman <merabpyh@yandex.ru>
2017-11-02 09:43:33 +0300

    Update build.sh

--- golang-compiler-alpine/build.sh
+++ golang-compiler-alpine/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Run from the directory with Dockerfile
-
-#echo "Create output dir"
-if [ ! -d "output" ]; then mkdir output; fi
-
 #echo "Build image"
 docker build -t alpine.go.compiler:latest .
